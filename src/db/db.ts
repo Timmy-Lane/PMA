ffe66b9d18e4139af81fae63df53f30960d553af
durden <idontloveyouanymore1@mail.ru>
2025-12-28 02:16:37 +0500

    Remove database connection and schema files
    Deleted src/db/db.ts and src/db/schema.ts, removing the database connection setup and schema definitions. This may be in preparation for a refactor or migration to a different database solution.

--- src/db/db.ts
+++
@@ -1,9 +0,0 @@
-import { drizzle } from 'drizzle-orm/node-postgres'
-import * as schema from './schema'
-import { Pool } from 'pg'
-
-const pool = new Pool({
-   connectionString: process.env.DB_URL!,
-})
-
-export const db = drizzle(pool, { schema })
